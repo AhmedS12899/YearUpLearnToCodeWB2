0880b1a2bf0f55c50a07a19ac5835e0154d06889
AhmedS12899 <AhmedS12899@Gmail.com>
2022-10-19 11:27:10 -0400

    formula for mortgage monthly/total interest

--- hello.js
+++ hello.js
@@ -1,15 +1,19 @@
-var message = "Hello World";
-console.log(message + '\n');
+// Mortgage Calc Formulas
 
-for(let i = 0; i<10; i++){
-    if(i%2==0){
-        console.log(i);
-    }
-}
+var principal = 53000;
+var years = 15;
+var interest = 0.07625/12;
+// M = P [ i(1 + i)^n ] / [ (1 + i)^n – 1] - monthly payment
+var monthly = (principal * ( interest*Math.pow((1+interest), 12*years) ) / ( Math.pow( (1+interest), (12*years)) -1 ));
+var totalInterest = 0;
+var principalTotal = principal;
+var monthlyInterest = 0;
+console.log("Monthly Payment is :"  + monthly.toFixed(2));
 
-let k=0;
-while(k<10){
-    if(k%2==0)
-    console.log(k);
-    k++;
+//Total Interest calculation
+for(let i = years*12; i>0 ;i--){
+    monthlyInterest = (principalTotal * interest);
+    totalInterest += monthlyInterest;
+    principalTotal = (principalTotal - (monthly - monthlyInterest));
 }
+console.log("Total Interest is : " + totalInterest.toFixed(2));
